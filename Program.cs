6bd8ca37f33d716d13d286499907839a4fc95ae3
John Souza <john5souz4@gmail.com>
2023-08-16 12:32:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TheCalculator
+{
+    class Program
+    {
+
+        enum Menu
+        {
+            ADDITION = 1,
+            SUBTRACTION,
+            MULTIPLICATION,
+            DIVISION,
+            EXPONENTIATION,
+            ROOT,
+            EXIT = 0
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            bool choseExit = false;
+            while (!choseExit)
+            {
+                Console.WriteLine("WELCOME TO THE C#LCULATOR\n");
+
+                Console.WriteLine
+                (
+                    "[1] ADDITION\n" +
+                    "[2] SUBTRACTION\n" +
+                    "[3] MULTIPLICATION\n" +
+                    "[4] DIVISION\n" +
+                    "[5] EXPONENTIATION\n" +
+                    "[6] ROOT\n" +
+                    "[0] EXIT\n"
+                );
+
+                Console.WriteLine("TYPE THE OPÇÃO");
+
+                Menu option = (Menu)int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case Menu.ADDITION:
+                        Addition();
+                        break;
+
+                    case Menu.SUBTRACTION:
+                        Subtraction();
+                        break;
+
+                    case Menu.MULTIPLICATION:
+                        Multiplication();
+                        break;
+
+                    case Menu.DIVISION:
+                        Division();
+                        break;
+
+                    case Menu.EXPONENTIATION:
+                        Exponentiation();
+                        break;
+
+                    case Menu.ROOT:
+                        Root();
+                        break;
+
+                    case Menu.EXIT:
+                        choseExit = true;
+                        break;
+                }
+
+                Console.WriteLine(option);
+                Console.Clear();
+
+            }
+        }
+
+        static void Addition()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO ADDITION\n");
+
+            Console.Write("TYPE THE FIRST: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("TYPE THE SECOND: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a + b;
+
+            Console.Write($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+
+        static void Subtraction()
+        {
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO SUBTRACTION\n");
+
+            Console.Write("TYPE THE FIRST: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("TYPE THE SECOND: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a - b;
+
+            Console.WriteLine($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+
+        static void Multiplication()
+        {
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO MULTIPLICATION\n");
+
+            Console.Write("TYPE THE FIRST: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("TYPE THE SECOND: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a * b;
+
+            Console.WriteLine($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO DIVISION\n");
+
+            Console.Write("TYPE THE FIRST: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("TYPE THE SECOND: ");
+            int b = int.Parse(Console.ReadLine());
+
+            float result = (float)a / (float)b;
+
+            Console.WriteLine($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+
+        static void Exponentiation()
+        {
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO EXPONENTIATION\n");
+
+            Console.Write("TYPE THE BASE: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("TYPE THE EXPONENTIATION: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = (int)Math.Pow(a, b);
+
+            Console.WriteLine($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+
+        static void Root()
+        {
+            Console.Clear();
+
+            Console.WriteLine("WELCOME TO ROOT\n");
+
+            Console.Write("TYPE THE ROOT: ");
+            int a = int.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(a);
+
+            Console.WriteLine($"THE RESULT IS: {result}\n");
+
+            Console.Write("TYPE ENTER TO RUN TO MENU...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# TheC#lculator Documentation
+
+---
+
+**Introduction**
+Welcome to TheC#lculator, a comprehensive console-based application offering essential arithmetic operations for users. With an intuitive interface and straightforward functionality, users can easily perform operations like addition, subtraction, multiplication, division, exponentiation, and even root calculations.
+
+## Code Overview
+
+1. **Enum Menu**:
+   The application defines an enumeration `Menu` for better code readability and efficient menu item management.
+
+2. **Main()**:
+   The main method showcases a loop where users can choose the arithmetic operation they want to perform. The loop continues until the user selects the EXIT option.
+
+3. **Addition(), Subtraction(), Multiplication(), Division(), Exponentiation(), Root()**:
+   Each of these methods corresponds to a unique arithmetic operation. Users input the necessary values, and the application promptly displays the result.
+
+## Future Refactorings
+It's essential to understand that this code is slated for an update in the future. The objective of this refactoring will be to enhance performance, improve user interaction, and potentially introduce new features, taking into account user feedback.
+
+## How to Use
+Using TheC#lculator is simple:
+
+- Clone the repository to your local machine.
+- Open your terminal or command prompt and navigate to the directory containing the C# file.
+- If you have the .NET SDK installed, use the command `dotnet run <filename>.cs` to run the program.
+- Follow on-screen prompts to choose and execute an arithmetic operation.
+
+## Feedback
+Your feedback is invaluable to us. If you've suggestions, issues, or improvements, please feel free to get in touch.
